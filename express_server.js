9643ea1c5d318a2943070533996eda3cc08055a6
Nibin Mathew <mathew.nibin89@gmail.com>
2021-07-05 22:11:15 +0000

    setting EJS

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,7 +10,6 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
