75661ff5286bb67f5414f2eab21b1196ee59de37
Nibin Mathew <mathew.nibin89@gmail.com>
2021-07-06 15:58:33 +0000

    updated restart server logic with nodemon

--- express_server.js
+++ express_server.js
@@ -3,6 +3,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Got this function from google search
+function generateRandomString() {
+  var result           = '';
+  var characters       = 'AbC3g6hijklmnopqrz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ console.log(result);
+ return result;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,10 +38,32 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  // generate random string == short url
+    const shortURL = generateRandomString(); 
+  // add short and URl as the key value pairs as the database
+  urlDatabase[shortURL] = req.body.longURL
+  // res.redirect /urls/+shorturl
+  res.redirect(`/urls/${shortURL}`);
+      
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
