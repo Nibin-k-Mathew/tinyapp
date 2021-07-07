9be94340f35ff748f990e90fe4d02caf07818b56
Nibin Mathew <mathew.nibin89@gmail.com>
2021-07-07 05:55:26 +0000

    added cookies changes

--- express_server.js
+++ express_server.js
@@ -2,21 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser")
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 //Got this function from google search
 function generateRandomString() {
-  var result           = '';
-  var characters       = 'AbC3g6hijklmnopqrz0123456789';
+  var result = '';
+  var characters = 'AbC3g6hijklmnopqrz0123456789';
   var charactersLength = characters.length;
-  for ( var i = 0; i < 6; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- console.log(result);
- return result;
+  for (var i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  console.log(result);
+  return result;
 }
 
 const urlDatabase = {
@@ -33,13 +36,30 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Login form in Tiny app
+app.post("/login", (req, res) => {
+  console.log("Save this is the req",req)
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -48,36 +68,37 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log("message from post",req.body);  // Log the POST request body to the console
+  console.log("message from post", req.body);  // Log the POST request body to the console
   // generate random string == short url
-    const shortURL = generateRandomString(); 
-    const prefix = 'https://';
-    if(req.body.longURL.startsWith(prefix)){
-      urlDatabase[shortURL] = req.body.longURL
-    }
-    else {
-      urlDatabase[shortURL] = `${prefix}${req.body.longURL}`
-    }
+  const shortURL = generateRandomString();
+  const prefix = 'https://';
+  if (req.body.longURL.startsWith(prefix)) {
+    urlDatabase[shortURL] = req.body.longURL
+  }
+  else {
+    urlDatabase[shortURL] = `${prefix}${req.body.longURL}`
+  }
   // add short and URl as the key value pairs as the database
   // res.redirect /urls/+shorturl
   res.redirect(`/urls/${shortURL}`);
-      
+
 });
 
 //Delete POST/urls/:id/delete
-app.post('/urls/:shortURL/delete', (req,res) => {
+app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+  console.log("req delete", req.body)
   res.redirect("/urls");
 
 });
 // Updating the long URL
-app.post('/urls/:shortURL', (req,res) => {
+app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  console.log('POST /urls/:shortURL',shortURL);
-  console.log("LongURL",longURL);
-  urlDatabase[shortURL] = longURL; 
+  console.log('POST /urls/:shortURL', shortURL);
+  console.log("LongURL", longURL);
+  urlDatabase[shortURL] = longURL;
   res.redirect("/urls");
 })
 
@@ -85,17 +106,17 @@ app.post('/urls/:shortURL', (req,res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  console.log("Short",shortURL);
+  console.log("Short", shortURL);
   const longURL = urlDatabase[shortURL];
-  console.log("long",longURL);
-  if(longURL === undefined){
+  console.log("long", longURL);
+  if (longURL === undefined) {
     res.status(404)
     res.send("ShortURL doesnot exist")
   }
   else {
     res.redirect(longURL);
   }
-    
+
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
