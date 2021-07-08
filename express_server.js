a8e94502a04859f29394ddae2a8e3709bab1985b
Nibin Mathew <mathew.nibin89@gmail.com>
2021-07-08 02:15:14 +0000

    code for Login and Logout

--- express_server.js
+++ express_server.js
@@ -26,9 +26,35 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = {
+  "id": {
+    id: "id",
+    email: "a@example.com",
+    password: "1234"
+  },
+  "ID2": {
+    id: "ID2",
+    email: "b@example.com",
+    password: "123"
+  }
+};
+
+// find user by email helper function
+const findUserByEmail = (email) => {
+//if we have a user, return the user
+//if not return null.
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return null;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -36,35 +62,37 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//Login form in Tiny app
-//no longer need username cookie/deprecate, instead of we renders user_id cookie
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
-});
 //LogOut form in Tiny app
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
+  //update the username object with user_id
+  const user_id = req.cookies['user_id'];
+  console.log("******",user_id);
+  let user;
+  if (user_id) {
+    user = users[user_id];
+  }
   const templateVars = {
-    username: req.cookies["username"],
+    user,
     urls: urlDatabase
   };
+  console.log("templateVars",templateVars.user);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: req.cookies["user_ID"],
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],username :req.cookies["username"]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],user:null};
   res.render("urls_show", templateVars);
 });
 
@@ -82,6 +110,98 @@ app.post("/urls", (req, res) => {
   // res.redirect /urls/+shorturl
   res.redirect(`/urls/${shortURL}`);
 
+});
+
+//GET/login
+app.get('/login', (req,res) => {
+  const templateVars = {
+    user: null
+  };
+  res.render('urls_login',templateVars);
+});
+
+//GET/register
+app.get('/register', (req,res) => {
+  const templateVars = {
+    user:null
+  };
+  res.render('urls_registration',templateVars);
+});
+
+// POST/login
+app.post("/login", (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log("email is ",email);
+  console.log("Password is", password);
+  //add the new users in the users object
+  const id = Math.floor(Math.random() * 1000) + 1;
+
+  //If the e-mail or password are empty strings,
+  //send back a response with the 400 status code
+  if (!email || !password) {
+    return res.status(400).send("Email and password cannot be blank");
+  }
+  //If someone tries to register with an email that is already in the users object,
+  //send back a response with the 400 status code
+
+  const user = findUserByEmail(email);
+  console.log("user is",user);
+  if (!user) {
+    return res.status(403).send("Email not found");
+  } else if (user) {
+    if (user.password !== password) {
+      return res.status(403).send("password doesnt match");
+    }
+  }
+  users[id] = {
+    id,
+    email,
+    password
+  };
+  console.log("users id", users[id]);
+  res.cookie("user_id", id);
+  console.log("$$$$$$$$$$$", users);
+
+  res.redirect("/urls");
+
+});
+
+
+
+//POST/register
+app.post("/register", (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log("email", email);
+  console.log("password",password);
+  // add the new users in the users object
+  const id = Math.floor(Math.random() * 1000) + 1;
+  if (!email || !password) {
+    return res.status(400).send("Email and password cannot be blank");
+  }
+  //If someone tries to register with an email that is already in the users object,
+  //send back a response with the 400 status code
+
+  const user = findUserByEmail(email);
+  console.log(user);
+  if (user) {
+    return res.status(400).send("Email already in use");
+  }
+  users[id] = {
+    id,
+    email,
+    password
+  };
+  console.log("users id", users[id]);
+  res.cookie("user_id", id);
+  console.log("$$$$$$$$$$$", users);
+
+  //If the e-mail or password are empty strings,
+  //send back a response with the 400 status code
+
+  res.redirect("/urls");
 });
 
 //Delete POST/urls/:id/delete
